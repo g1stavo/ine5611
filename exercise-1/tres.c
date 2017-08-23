43a46ff18522495846d5d75f64ad697feccce18f
Gustavo <gustavo.salvador@chaordicsystems.com>
2017-08-23 02:53:00 -0300

    Tres (#2)
    * Improved dois.c
    
    * Improved dois.c
    
    * Tres completed

---
+++ exercise-1/tres.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void main() {
+  int i = 0;
+  while (i < 2) {
+    i++;
+    if (fork()) {
+      printf("%i\n", getpid());
+      wait(NULL);
+    } else {
+      printf("Processo %i filho de %i\n", getpid(), getppid());
+      if (fork()) {
+          wait(NULL);
+      } else {
+        printf("Processo %i filho de %i\n", getpid(), getppid());
+        if (fork()) {
+            wait(NULL);
+        } else {
+          printf("Processo %i filho de %i\n", getpid(), getppid());
+        }
+      }
+      return;
+    }
+  }
+}
