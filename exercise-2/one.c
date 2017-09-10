59dc0c5abf21d42a76fcbfa4956aa168b6a38525
gustavocsalvador <gustavocsalvador@gmail.com>
2017-09-10 12:36:19 -0300

    First two exercises OK

---
+++ exercise-2/one.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <pthread.h>
+
+void *work() {
+  printf("Nova thread criada. TID = %u!\n", (unsigned int)pthread_self());
+  pthread_exit(NULL);
+}
+
+void main() {
+  pthread_t worker;
+  pthread_create(&worker, NULL, work, NULL);  // cria thread
+  pthread_join(worker, NULL); // aguarda finalização da execução de work()
+  pthread_exit(NULL);
+}
